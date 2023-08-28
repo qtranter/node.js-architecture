47c5dfebd3e7a492dc308616e2536270cf19612c
quaide <tranterquaide@gmail.com>
2023-08-27 20:45:55 -0400

    clean structure

---
+++ README.md
@@ -0,0 +1,55 @@
+# node.js-clean-architecture
+A use case of Clean Architecture in Node.js comprising of Express.js, MongoDB and Redis as the main (but replaceable) infrastructure.
+
+### Overview
+This example is a simple RESTful API application in which a user can create / update / delete / find a post, by using the *Clean Architecture*.
+
+The objective of *Clean Architecture* by [Robert C. Martin] is the separation of concerns in software. 
+This separation is achieved by dividing the software into layers. Each layer is encapsulated by a higher level layer and the way to communicate between the layers is with the *Dependency Rule*.
+
+![](https://blog.cleancoder.com/uncle-bob/images/2012-08-13-the-clean-architecture/CleanArchitecture.jpg)
+
+#### Dependency Rule
+This rule says that nothing in an inner circle can know anything at all about something in an outer circle. The dependency direction is from the outside in. Meaning that the *Entities* layer is independent and the *Frameworks & Drivers* layer (Web, UI, DB etc.) depends on all the other layers.
+#### Entities
+Contains all the business entities an application consists of. In our example the *User* and the *Post*.
+#### Use Cases
+Contains application specific business rules. These use cases orchestrate the flow of data to and from the entities. In our example some of the use cases are: *AddPost*, *AddUser*, *DeleteById* etc.
+#### Interface Adapters
+This layer is a set of adapters (controllers, presenters, and gateways) that convert data from the format most convenient for the use cases and entities, to the format most convenient for some external agency such as the DB or the Web. In other words, is an entry and exit points to the Use Cases layer. In our example we implemented controllers and presenters together and these are the PostController and the UserController.
+#### Frameworks and Drivers
+The outermost layer is generally composed of frameworks and tools such as the Database, the Web Framework, etc.
+### How to run it
+* Make sure you have [mongoDB] installed. At the terminal run the following command:
+```sh
+mongod --dbpath <path_to_data/db_folder>
+```
+* Make sure [Redis] is also installed and running.<br /><br />
+* Run the server in development mode by typing the following command:
+```sh
+npm run dev
+```
+* Run the server in production mode by typing the following command:
+```sh
+npm run start
+```
+
+### How to run it (using Docker)
+* Make sure you have [docker] installed. At the root folder run the following command:
+```sh
+docker-compose up -d
+```
+### API documentation
+https://documenter.getpostman.com/view/1551953/TzCJgpnb
+
+
+### Further reading
+- https://roystack.home.blog/2019/10/22/node-clean-architecture-deep-dive/
+- https://mannhowie.com/clean-architecture-node
+
+[Robert C. Martin]: <https://blog.cleancoder.com/uncle-bob/2012/08/13/the-clean-architecture.html>
+[docker]: <https://www.docker.com/>  
+[mongoDB]: <https://www.mongodb.com/try/download/community>
+[Redis]: <https://redis.io/download>
+   
+   
---
+++ tests/unit/post/use_cases/use_cases.spec.test.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-unused-expressions */
+import chai from 'chai';
+import sinon from 'sinon';
+import faker from 'faker';
+
+import post from '../../../../src/entities/post';
+import addPost from '../../../../application/use_cases/post/add';
+import findAll from '../../../../application/use_cases/post/findAll';
+import findById from '../../../../application/use_cases/post/findById';
+import postDbRepository from '../../../../application/repositories/postDbRepository';
+
+const { expect } = chai;
+
+let dbRepository = null;
+
+describe('Use cases', () => {
+  beforeEach(() => {
+    dbRepository = postDbRepository();
+  });
+
+  describe('Fetch a specific post', () => {
+    it('should fetch a post by id', () => {
+      const stubPost = {
+        title: faker.name.findName(),
+        description: faker.name.findName(),
+        createdAt: faker.date.past(),
+        isPublished: false,
+        userId: faker.random.uuid()
+      };
+      const correspondingPost = post({
+        title: stubPost.title,
+        description: stubPost.description,
+        createdAt: stubPost.createdAt,
+        isPublished: stubPost.isPublished,
+        userId: stubPost.userId,
+        postRepository: dbRepository
+      });
+      const stubRepositoryFindById = sinon
+        .stub(dbRepository, 'findById')
+        .returns(correspondingPost);
+      const fetchedPost = findById('5fb1b12a6ac3e23493ac82e4', dbRepository);
+      expect(stubRepositoryFindById.calledOnce).to.be.true;
+      sinon.assert.calledWith(
+        stubRepositoryFindById,
+        '5fb1b12a6ac3e23493ac82e4'
+      );
+      expect(fetchedPost).to.eql(correspondingPost);
+    });
+  });
+
+  describe('Fetch all posts', () => {
+    it('should fetch all the posts succesfully', () => {
+      const stubRepositoryFetchAll = sinon
+        .stub(dbRepository, 'findAll')
+        .returns(['post1', 'post2']);
+      const posts = findAll('602c13e0cfe08b794e1b287b', dbRepository);
+      expect(stubRepositoryFetchAll.calledOnce).to.be.true;
+      expect(posts).to.eql(['post1', 'post2']);
+    });
+  });
+
+  describe('Add new post', () => {
+    it('should add a new post succesfully', () => {
+      const stubValue = {
+        title: faker.name.findName(),
+        description: faker.name.findName(),
+        createdAt: faker.date.past(),
+        isPublished: false,
+        userId: faker.random.uuid()
+      };
+      const pesristedPost = post({
+        title: stubValue.title,
+        description: stubValue.description,
+        createdAt: stubValue.createdAt,
+        isPublished: stubValue.isPublished,
+        userId: stubValue.userId
+      });
+      const stubRepositoryAdd = sinon
+        .stub(dbRepository, 'add')
+        .returns(pesristedPost);
+      const newPost = addPost({
+        title: stubValue.title,
+        description: stubValue.description,
+        createdAt: stubValue.createdAt,
+        isPublished: stubValue.isPublished,
+        userId: stubValue.userId,
+        postRepository: dbRepository
+      });
+      expect(stubRepositoryAdd.calledOnce).to.be.true;
+      expect(newPost.getTitle()).equals(stubValue.title);
+      expect(newPost.getDescription()).equals(stubValue.description);
+      expect(newPost.getCreatedAt()).equals(stubValue.createdAt);
+      expect(newPost.isPublished()).equals(stubValue.isPublished);
+      expect(newPost.getUserId()).equals(stubValue.userId);
+    });
+  });
+});
